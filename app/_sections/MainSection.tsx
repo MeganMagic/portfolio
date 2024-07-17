6cde14701edfb0a32df39ee5a95ef4d247576157
MeganMagic <at.fence.3@gmail.com>
2024-07-17 21:02:47 +0900

    feat: Main section 애니메이션 구현 - 1.삼각형

--- app/_sections/MainSection.tsx
+++ app/_sections/MainSection.tsx
@@ -1,22 +1,20 @@
-import Image from "next/image";
+"use client";
+
+import Triangle from "@/_components/MotionShapes/Triangle";
 
 const MainSection = () => {
   return (
     <div id="main" className="w-full pt-16 pb-24 flex flex-col gap-10 md:gap-16">
-      <h1>
+      <h1 className="text-center">
+        안녕하세요,
+        <br />
         프론트엔드 개발자
         <br />
-        <em>송진경</em> 입니다.
+        <em>송진경</em>입니다.
       </h1>
 
       <div className="w-full h-32 md:h-48 relative">
-        <Image
-          className="object-contain object-left"
-          src="/assets/hero-image.png"
-          alt="triangle, circle, and rectangle"
-          fill
-          priority
-        />
+        <Triangle />
       </div>
     </div>
   );
