ab90d1e22ba72f295e7b90f4c8d0c0e0b9074134
MeganMagic <at.fence.3@gmail.com>
2024-07-27 23:54:53 +0900

    feat: Main Section 애니메이션 추가, 변수 정리

--- app/_components/MotionShapes/constants.ts
+++ app/_components/MotionShapes/constants.ts
@@ -1,26 +1,42 @@
-export const beforeStart = 2;
-export const startStagger = 0.5;
+import { Easing, Spring } from "framer-motion";
+type SpringType = Spring["type"];
 
-const durationOffset = 0.5;
+export const scene1 = {
+  duration: 0.5,
+  at: 0.5,
+  type: "spring" as SpringType,
+  damping: 7,
+  bounce: 0.25,
+};
+export const scene1Stagger = 0.1;
+
+export const scene2 = {
+  at: 2,
+  duration: 0.5,
+  ease: "easeInOut" as Easing,
+  type: "spring" as SpringType,
+};
+
+export const scene3at = 3;
 const getTotalDuration = (obj: { [k in string]: number }) => {
+  const durationOffset = 0.5;
   return Object.entries(obj).reduce((acc, cur) => acc + cur[1], 0) - durationOffset;
 };
-
 // first shape
-export const shape1 = {
+export const scene3shape1Durations = {
   pathLength1: 1,
 };
-export const shape1Duration = getTotalDuration(shape1);
+export const scene3shape1TotalDuration = getTotalDuration(scene3shape1Durations);
 
 // second shape
-export const shape2 = {
+export const scene3shape2Durations = {
   moveY1: 0.5,
   pathLength1: 1,
 };
-export const shape2Duration = getTotalDuration(shape2);
+export const scene3shape2TotalDuration = getTotalDuration(scene3shape2Durations);
 
 // third shape
-export const shape3 = {
+export const scene3shape3Durations = {
   moveX1: 0.5,
   pathLength1: 1,
   scaleUp1: 0.5,
