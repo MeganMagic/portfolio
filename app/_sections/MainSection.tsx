5e8c8a14b5bc0cd026e10636e86e7ec5b081fe40
MeganMagic <at.fence.3@gmail.com>
2024-07-27 23:55:31 +0900

    Feat: Header 수정, Section 위치에 따른 active 상태 구현

--- app/_sections/MainSection.tsx
+++ app/_sections/MainSection.tsx
@@ -6,8 +6,14 @@ import ThirdShape from "@/_components/MotionShapes/ThirdShape";
 
 const MainSection = () => {
   return (
-    <div id="main" className="w-full pt-16 pb-24 flex flex-col gap-10 md:gap-16">
-      <h1 className="text-center">
+    <div id="main" className="w-full pt-16 pb-24 flex flex-col">
+      <div className="w-full h-32 md:h-48 relative flex justify-center scale-50 origin-bottom">
+        <FirstShape />
+        <SecondShape />
+        <ThirdShape />
+      </div>
+
+      <h1 className="w-full p-6 md:p-8 text-center bg-white z-40">
         안녕하세요,
         <br />
         프론트엔드 개발자
@@ -15,11 +21,13 @@ const MainSection = () => {
         <em>송진경</em>입니다.
       </h1>
 
-      <div className="w-full h-32 md:h-48 relative flex justify-center">
-        <FirstShape />
-        <SecondShape />
-        <ThirdShape />
-      </div>
+      <p className="text-center text-sm md:text-base font-normal text-gray-500">
+        React, Typescript, Next.js 기반의 웹 프론트엔드를 개발합니다.
+        <br />
+        2년 이상의 업무 경험이 있으며, 다양한 직군과 원활한 커뮤니케이션이 가능합니다.
+        <br />
+        좋은 제품을 만들고 성장시키는 일에 함께하고 싶습니다.
+      </p>
     </div>
   );
 };
