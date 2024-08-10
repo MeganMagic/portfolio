24a7113de7105ca4ce1919d6bd9d526e99509486
송진경 <53007682+MeganMagic@users.noreply.github.com>
2024-08-10 13:11:59 +0900

    [8월 1주차] 메인섹션 애니메이션 오류 수정, DB column 추가 (#6)
    * fix: safari에서 발생하는 main section 애니메이션 오류 수정
    
    * fix: section title UI 수정
    
    * feat: experience 섹션에 순서 column(index) 추가
    
    * feat: Skill 테이블 column 추가, category enum에 value 추가

--- app/_sections/ExperienceSection.tsx
+++ app/_sections/ExperienceSection.tsx
@@ -5,7 +5,7 @@ import prisma from "@/lib/prisma";
 import { parsePrismaJSON } from "@/utils/parsePrisma";
 
 async function getExperience() {
-  const response = await prisma.experience.findMany({ orderBy: { id: "asc" } });
+  const response = await prisma.experience.findMany({ orderBy: { index: "asc" } });
 
   return response.map(({ links, sub_title, is_active, ...res }) => ({
     subTitle: sub_title ?? undefined,
@@ -22,7 +22,7 @@ export default async function ExperienceSection() {
     <SectionWatcher id="experience">
       <SlideUpInView>
         <h2>업무 및 프로젝트 경험</h2>
-        <p className="section-title">스타트업, 외주, 프로젝트 등 다양한 업무를 경험했습니다.</p>
+        <p className="section-description">스타트업, 외주, 프로젝트 등 다양한 업무를 경험했습니다.</p>
         <div className="flex flex-col gap-16 md:gap-20">
           {data.map(props => (
             <ExpCard key={`exp-card-${props.id}`} {...props} />
