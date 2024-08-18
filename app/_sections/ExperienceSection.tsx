15106dcd36426152f796c8a55edec6b2509240d1
MeganMagic <at.fence.3@gmail.com>
2024-08-18 16:50:50 +0900

    feat: Experience 섹션 수정

--- app/_sections/ExperienceSection.tsx
+++ app/_sections/ExperienceSection.tsx
@@ -1,15 +1,30 @@
+import React from "react";
+
 import ExpCard from "@/_components/ExpCard";
 import SectionWatcher from "@/_components/SectionWatcher";
 import SlideUpInView from "@/_components/SlideUpInView";
 import prisma from "@/lib/prisma";
 import { parsePrismaJSON } from "@/utils/parsePrisma";
 
-async function getExperience() {
-  const response = await prisma.experience.findMany({ orderBy: { index: "asc" } });
+async function getSkills(ids: number[]) {
+  const response = await prisma.skill.findMany({
+    where: { OR: ids.map(id => ({ id })) },
+    orderBy: { category: "asc" },
+  });
+  return response;
+}
 
-  return response.map(({ links, sub_title, is_active, ...res }) => ({
-    subTitle: sub_title ?? undefined,
-    onGoing: is_active ?? false,
+async function getExperience() {
+  const experiences = await prisma.experience.findMany({ orderBy: { index: "asc" } });
+
+  const expWithSkills = await Promise.all(
+    experiences.map(async ({ skill_ids, ...exp }) => {
+      const skills = await getSkills(skill_ids);
+      return { ...exp, skills };
+    }),
+  );
+
+  return expWithSkills.map(({ links, ...res }) => ({
     links: links.map(link => parsePrismaJSON<{ href: string; label: string }>(link)),
     ...res,
   }));
@@ -18,16 +33,37 @@ async function getExperience() {
 export default async function ExperienceSection() {
   const data = await getExperience();
 
+  const works = data.filter(({ category }) => category === "WORK");
+  const projects = data.filter(({ category }) => category === "PROJECT");
+
   return (
     <SectionWatcher id="experience">
       <SlideUpInView>
-        <h2>업무 및 프로젝트 경험</h2>
-        <p className="section-description">스타트업, 외주, 프로젝트 등 다양한 업무를 경험했습니다.</p>
-        <div className="flex flex-col gap-16 md:gap-20">
-          {data.map(props => (
-            <ExpCard key={`exp-card-${props.id}`} {...props} />
-          ))}
-        </div>
+        <h2 className="section-eyebrow">경력 사항</h2>
+        <p className="section-title">
+          다양한 업무와 프로젝트를 통해
+          <br />
+          경험과 노하우를 쌓고 있습니다.
+        </p>
+
+        {[
+          { title: "업무 경험", data: works },
+          { title: "프로젝트", data: projects },
+        ].map(({ title, data }) => (
+          <React.Fragment key={`exp-${title}`}>
+            <div className="flex gap-4 items-center md:max-w-[768px] mx-auto mt-12 mb-8">
+              <div className="w-full h-[1px] bg-gradient-to-l from-foreground/15" />
+              <p className="flex-shrink-0 text-xs md:text-sm text-foreground/50">{title}</p>
+              <div className="w-full h-[1px] bg-gradient-to-r from-foreground/15" />
+            </div>
+
+            <div className="flex flex-col gap-8 md:gap-10">
+              {data.map(props => (
+                <ExpCard key={`exp-${title}-card-${props.id}`} {...props} />
+              ))}
+            </div>
+          </React.Fragment>
+        ))}
       </SlideUpInView>
     </SectionWatcher>
   );
